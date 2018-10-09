287e561883b7a360c88e1ad08d05cd748ddde9c0
Olga <parankinaolya@gmail.com>
2018-10-09 12:34:22 +0300

    'commit'

---
+++ src/js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+$('.single-item').slick( {dots: false} );
+
+});
+
+$(document).ready(function() {
+
+$('.single-item1').slick( {dots: true} );
+
+});
+
+$(document).ready(function() {
+
+$('.single-item2').slick( {dots: true} );
+
+});
